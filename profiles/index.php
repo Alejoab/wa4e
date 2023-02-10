3832e327804f86d1f8734e9656a308fa1b4ca84d
Alejoab <aalvarezbo@unal.edu.co>
2023-02-09 21:07:55 -0500

    Add flashing message to profiles/index.php

--- profiles/index.php
+++ profiles/index.php
@@ -27,6 +27,16 @@ session_start();
         }
         ?>
 
+        <?php
+            if (isset($_SESSION['success'])) {
+                echo ('<p class="alert alert-success">' . $_SESSION['success'] . "</p>\n");
+                unset($_SESSION['success']);
+            } elseif (isset($_SESSION['error'])) {
+                echo ('<p class="alert alert-danger">' . $_SESSION['error'] . "</p>\n");
+                unset($_SESSION['error']);
+            }
+        ?>
+
         <div class="col-10 px-2">
             <?php
             require_once("pdo.php");
